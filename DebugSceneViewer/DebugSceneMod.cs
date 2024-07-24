554eadf14d283a0ab0b4a7e0f45b3fd30abdce43
synth <wavydave2000@gmail.com>
2024-07-24 00:42:10 -0400

    Add project files.

---
+++ DebugSceneViewer/DebugSceneMod.cs
@@ -0,0 +1,21 @@
+﻿using KHMI;
+using KHMI.Types;
+
+namespace DebugSceneViewer
+{
+    public class DebugSceneMod : KHMod
+    {
+        public DebugSceneMod(ModInterface mi) : base(mi)
+        {
+            for(int i = 0; i <= 16; i++)
+            {
+                World currentWorld = new World(modInterface.dataInterface, i);
+                Room[] currentRooms = currentWorld.Rooms;
+                foreach(Room room in currentRooms)
+                {
+                    room.SceneID = 25;
+                }
+            }
+        }
+    }
+}
